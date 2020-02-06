1290cb25bc2c2dab3183de7548db3152cf3af5ad
Ajmal Noushad <ajufhn@gmail.com>
2020-02-06 10:54:04 +0530

    Updates

---
+++ precache-manifest.0817b9ea0bf813f35522b662f9e33081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0786a94e2e55a2f25620ca95e03b93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681fd5c2d0d9a486cb47",
+    "url": "/static/css/main.0b29fabe.chunk.css"
+  },
+  {
+    "revision": "1a931034aa481e291b1d",
+    "url": "/static/js/2.0564e04c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0564e04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681fd5c2d0d9a486cb47",
+    "url": "/static/js/main.9c6f9654.chunk.js"
+  },
+  {
+    "revision": "1b9d0c6a20efbf771c49",
+    "url": "/static/js/runtime-main.b60bffe4.js"
+  }
+]);
